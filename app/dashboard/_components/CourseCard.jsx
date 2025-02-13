89c61042b5dc1315ab5982837bd47e467c81dc6e
Saksham Agarwal <sakshamagarwal0507@gmail.com>
2025-02-13 20:58:14 +0530

    Explore page done part-19

--- app/dashboard/_components/CourseCard.jsx
+++ app/dashboard/_components/CourseCard.jsx
@@ -14,12 +14,12 @@ import { eq } from 'drizzle-orm';
 import Link from 'next/link';
 
 
-function CourseCard({ course, refreshData }) {
+function CourseCard({ course, refreshData, displayUser = false }) {
 
     const handleDelete = async () => {
         const res = await db.delete(CourseList).where(eq(CourseList.id, course?.id))
             .returning({ id: CourseList?.id })
- 
+
         if (res) {
             refreshData()
         }
@@ -27,22 +27,25 @@ function CourseCard({ course, refreshData }) {
 
     return (
         <div className='shadow-sm rounded-lg border p-2 cursor-pointer mt-4'>
-            <Link href={'/course/'+ course?.courseId}>
+            <Link href={'/course/' + course?.courseId}>
                 <Image src={course?.courseBanner} width={300} height={200}
                     className='w-full h-[200px] object-cover rounded-lg' />
             </Link>
             <div className='p-2'>
                 <h2 className='font-medium text-lg flex justify-between items-center'>
                     {course?.courseOutput?.CourseName}
-                    <DropDownOption handleDelete={() => handleDelete()}>
+                    {!displayUser && <DropDownOption handleDelete={() => handleDelete()}>
                         <HiMiniEllipsisVertical />
-                    </DropDownOption>
+                    </DropDownOption>}
                 </h2>
                 <p className='text-sm text-gray-400 my-1'>{course?.category}</p>
                 <div className='flex items-center justify-between'>
                     <h2 className='flex gap-2 items-center p-1 bg-blue-50 text-blue-500 text-sm rounded-sm'><HiOutlineBookOpen />{course?.courseOutput?.NoOfChapters} Chapters</h2>
                     <h2 className='text-sm p-1 bg-blue-50 text-blue-500 rounded-sm'>{course?.level}</h2>
                 </div>
+                {displayUser && <div className='mt-2'>
+                    <h2 className='text-sm'><b>Created By: </b>{course?.userName}</h2>
+                </div>}
             </div>
         </div>
     )
